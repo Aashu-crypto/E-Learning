af2b42e5c47a25458860b6516088fd2a421f36ee
Aashu-crypto <ashugandotra14@gmail.com>
2024-08-16 11:58:46 +0530

    deleted turbo repo

--- apps/backend/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-const courseRouter = require("./CourseRoutes");
-const userRouter = require("./UserRoutes");
-
-router.use("/user", userRouter);
-router.use("/course", courseRouter);
-
-export default router;
---
+++ backend/src/routes/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+// const courseRouter = require("./CourseRoutes");
+const userRouter = require("./UserRoutes");
+
+router.use("/user", userRouter);
+// router.use("/course", courseRouter);
+
+export default router;
